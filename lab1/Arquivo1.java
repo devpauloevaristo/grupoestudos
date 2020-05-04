25254c871da85e5dbe2faa8187c765a208599be4
Paulo Evaristo <pauloevaristocba@gmail.com>
2020-05-04 15:29:26 -0400

    Alterada a classe Arquivo1.java

--- lab1/Arquivo1.java
+++ lab1/Arquivo1.java
@@ -1,6 +1,6 @@
 package lab1;
 public class Arquivo1 {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        System.out.println("Olá mundo! Esta é minha primeira classe java");
     }
 }
